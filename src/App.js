563230898b641fa3969c215811c5c4b43aa6839f
Osman Rasooli <mohammad.osmanrasooli1973@gmail.com>
2022-11-05 02:05:02 +0430

    File Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <span>Subscribe Zainkeepscode</span>
+      <span>OSMAN</span>
     </div>
   );
 }
